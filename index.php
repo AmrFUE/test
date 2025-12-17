e3090adf2b0a35928dfe782664a76e47e9ef09a8
AmrAshour <114658676+AmrFUE@users.noreply.github.com>
2025-12-17 13:34:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+ob_start();
+session_start();
+?>
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">
+					<?php include('head.php'); ?>
+				
+				<div class="span10">
+					<?php include('slider.php'); ?>
+				</div>
+			
+				<div class="span2">
+				<h4></h4>
+			
+				</div>
+				
+			
+			</div>
+		</div>
+    </div>
+<?php include('footer.php') ?>
+<?php ob_end_flush(); ?>
